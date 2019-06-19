27931fd466db84009c33ab3efc946583af6b3cad
cmac <chelsey.macneill@gmail.com>
2019-06-18 18:12:24 -0700

    Buy an item functiion added but not working

--- storefront.js
+++ storefront.js
@@ -3,7 +3,6 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 // Mysql database connection credentials (SQL Database is hosted on my locl machine)
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -53,17 +52,33 @@ function start() {
 function displayItems(){
     connection.query("SELECT * FROM products", function(err,res) {
         if (err) throw err;
-        console.log(res);
-        // Display the results nicely 
-        for (var i=0; i < res.length; i++) {
-            console.log("Item ID:" + res[i].item_id  + 
-            "\nItem Name:" + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].unit_count + " | " + res[i].stock_quantity
-            + "\n -----------------------")
-        }
+        // Display the results in a table for easy viewing 
+        console.table(res)
+        
         // re-prompt the user for what they want to do
         start();
+        
     })
 };
+
+// displaying the data in a table 
+function displayTable() {
+    connnection.query("SELECT * FROM products", function(err,res){
+        if (err) throw err; 
+        
+        // var table = new table({
+        //     head: ["item_id", "product_name", "department_name", "price", "stock_quantity"],
+        //     colWidths: [10,30,30,10,20],
+        //     style: {
+        //         head: ['green'],
+        //         compact: true
+        //     }
+        // })
+        
+        // display the table 
+        console.table(res)
+    })
+}
 
 // function that runs when a user wants to buy something
 function buyAnItem() {
@@ -99,17 +114,30 @@ function buyAnItem() {
         ])
         .then(function(answer) {
             // get information of the item they want to buy
-            console.log("This is happeninging")
             
-            console.log(answer.choice[0]) // This is the id of the item chosen
-            let chosenItem = answer.choice[0]
+            let chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if(results[i].item_id === answer.choice) {
+                    chosenItem = results[i];
+                }
+            };
+            
+            // testing things
+            console.log(chosenItem)
+            console.log("R" + results[i])
+            console.log(results)
+            console.log("Chosen Item" + chosenItem);
+            console.log("Chosen Item starting bid" + chosenItem.product_name)
+            
             // compare the quantity of how many they want to buy
             if (chosenItem.stock_quantity >= parseInt(answer.quantity)) {
+                console.log("This how much the person wants: " + answer.quantity)
+                console.log("This is the amount in stock: " + chosenItem.stock_quantity)
                 // bid was within what is in stock so purchase can go through
                 connection.query(
                     "UPDATE auctions SET ? WHERE ?",
                     [
-                        {stock_quantity: stock_quantity - answer.quantity},
+                        {stock_quantity: 5},
                         {item_id : chosenItem}
                     ],
                     function (error) {
@@ -119,6 +147,8 @@ function buyAnItem() {
                     });
                 }
                 else {
+                    console.log("This is the answer quantity in stock: " + answer.quantity)
+                    console.log("This is the amount in stock: " + chosenItem.stock_quantity)
                     // the quantity chosen was higher than what was in stock
                     console.log("There are only items in stock for  Try again.")
                     // propmpt them to try again
@@ -128,83 +158,7 @@ function buyAnItem() {
         })
     };
     
-    // Function to buy an item
-    function buy() {
-        connection.query("SELECT * FROM products", function(err, results){
-            if (err) throw err; 
-            // print out results 
-            
-            // once you have the items, prompt the user for which they'd like to bid on
-            inquirer
-            .prompt([
-                {
-                    name: "itemBuy",
-                    type : "input",
-                    message: "What item would you like to buy (Enter an item id) ?"
-                },
-                { 
-                    name: "orderAmount",
-                    type: "input",
-                    message: "How many would you like?"
-                }
-            ])
-            .then(function(answer) {
-                // based on the answer search the db for the item chosen
-                console.log("This is the answer :" + JSON.stringify(answer))
-                console.log("These are the results :" + JSON.stringify(results))
-                
-                console.log(answer);
-                console.log(answer.itemBuy.val)
-                
-                // Set chosen item equal to the product the customer chose
-                //let chosenItem = answer[]
-                
-                //make sure there are enough items for sale as the user wants
-                // if (chosenItem.stock_quantity >= parseInt(answer.orderAmount)) {
-                //     console.log("Order in stock");
-                //     // calculate new stock quantity 
-                //     let newStockQuantity = chosenItem.stock_quantity - parseInt(answer.orderAmount);
-                //     // there is enough in stock to fill the order
-                //     connection.query(
-                //         "UPDATE products SET ? WHERE ?", 
-                //         [
-                //             {
-                //                 stock_quantity : newStockQuantity
-                //             },
-                //             { id: chosenItem.item_id
-                //             }
-                //         ],
-                //         function(error) {
-                //             if (error) throw err;
-                //             console.log("Order placed successfully!");
-                //         });
-                //     }
-                //     else { 
-                //         // if the user wants more than what is in stock
-                //         console.log("There aren't that many in stock")
-                //     }
-            });
-        });
-    };
-    
-    
-    // (Admin only) Function for Admin only to add new products to the db
-    function createProduct() {
-        console.log("Inserting a new product...\n");
-        var query = connection.query(
-            "INSERT INTO products SET ?",
-            {
-                product_name: "Rocky Road",
-                price: 3.0,
-                stock_quantity: 50
-            },
-            function(err, res) {
-                if (err) throw err;
-                console.log(res.affectedRows + " product inserted!\n");
-                // Call updateProduct AFTER the INSERT completes
-                updateProduct();
-            }
-            )};
+
             
             
             
